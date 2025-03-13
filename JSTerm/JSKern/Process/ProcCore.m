2c673d3fbac94b9d5976f5a636d5759e2757b747
FridaDEV <157828245+fridakitten@users.noreply.github.com>
2025-03-13 20:15:57 +0100

    ProcCore update

--- JSTerm/JSKern/Process/ProcCore.m
+++ JSTerm/JSKern/Process/ProcCore.m
@@ -6,6 +6,8 @@
 //
 
 #import "ProcCore.h"
+#import <JSTerm-Swift.h>
+#include <stdint.h>
 
 /*
  @Brief thread for ProcCoreHelper
@@ -28,29 +30,38 @@ void* proccore_thread(void *args)
  */
 @implementation ProcCoreHelper
 
+BackIO *backio;
+
 - (instancetype)init {
     self = [super init];
     if (self) {
         _proc = [NSMutableDictionary dictionary];
+        backio = [[BackIO alloc] init];
     }
     return self;
 }
 
 - (void)assignThread:(uint16_t)pid thread:(pthread_t *)thread {
+    [backio osprintWithMsg:[NSString stringWithFormat: @"[ProcCore:%d] requestor requested attach thread %p to pid %d", pid, thread, pid]];
     self.proc[@(pid)] = [NSValue valueWithPointer:thread];
+    [backio osprintWithMsg:[NSString stringWithFormat: @"[ProcCore:%d] attached thread %p to pid %d", pid, thread, pid]];
 }
 
 - (pthread_t *)getThread:(uint16_t)pid {
+    [backio osprintWithMsg:[NSString stringWithFormat: @"[ProcCore:%d] requestor asked for the thread of pid %d", pid, pid]];
     NSValue *value = self.proc[@(pid)];
-    return value ? (pthread_t *)[value pointerValue] : NULL;
+    pthread_t *thread_ptr = value ? (pthread_t *)[value pointerValue] : NULL;
+    [backio osprintWithMsg:[NSString stringWithFormat: @"[ProcCore:%d] thread of %d is %p", pid, pid, thread_ptr]];
+    return thread_ptr;
 }
 
-- (void)run:(uint16_t)pid code:(NSString*)code ctx:(JSContext*)ctx jsargs:(NSArray*)jsargs {
+- (void)run:(uint16_t)pid code:(NSString*)code symbol:(NSString*)symbol ctx:(JSContext*)ctx jsargs:(NSArray*)jsargs {
+    [backio osprintWithMsg:[NSString stringWithFormat: @"[ProcCore:%d] requestor asked to run a task\nINFO\npid: %d\nsymbol: %@\ncontext:%p\njsagrs: %p", pid, pid, symbol, ctx, jsargs]];
     thread_args_js_t *args = malloc(sizeof(thread_args_js_t));
     args->code = code;
     args->ctx = ctx;
     args->args = jsargs;
-    args->symbol = @"main";
+    args->symbol = symbol;
     args->evaluate_code = YES;
     
     pthread_t *thread = malloc(sizeof(pthread_t));
@@ -62,9 +73,21 @@ void* proccore_thread(void *args)
     pthread_join(*thread, NULL);
 }
 
-- (void)kill:(uint16_t)pid {
+- (void)kill:(uint16_t)pid sema:(dispatch_semaphore_t)sema {
+    [backio osprintWithMsg:[NSString stringWithFormat: @"[ProcCore:%d] requestor requested to kill pid %d", pid, pid]];
     pthread_t *thread = [self getThread:pid];
-    pthread_cancel(*thread);
+    [backio osprintWithMsg:[NSString stringWithFormat: @"[ProcCore:%d] found thread %p for pid %d", pid, thread, pid]];
+    
+    int result = pthread_kill(*thread, 0);
+    [backio osprintWithMsg:[NSString stringWithFormat: @"[ProcCore:%d] killed thread %p with result %d", pid, thread, result]];
+    
+    if (result == 0) {
+        pthread_cancel(*thread);
+    } else {
+        [backio osprintWithMsg:[NSString stringWithFormat: @"[ProcCore:%d] looks like i wasnt able to do my job for pid %d", pid, pid]];
+    }
+    
+    dispatch_semaphore_signal(sema);
 }
 
 @end
